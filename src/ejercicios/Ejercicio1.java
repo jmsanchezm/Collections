9b0fe6f138937a7bb0d50711df460b0948008aab
jmmoreno <jmmoreno@106-15.info2>
2023-05-10 14:34:41 +0200

    Terminado y comentado

---
+++ src/ejercicios/Ejercicio1.java
@@ -0,0 +1,26 @@
+package ejercicios;
+
+import java.util.LinkedHashSet;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		//Creamos una coleccion de tipo LinkedHashSet que permita los tipo Integer
+		LinkedHashSet<Integer> coleccion = new LinkedHashSet<Integer>() ;
+		
+		int numero=0; //Se guardara el numero generado
+	
+		//Mientras que el tamaño de la coleccion sea menor que 20
+		while( coleccion.size() <20) {
+			//Generamos el número aleatorio
+			numero = (int)(Math.random()*101);
+			//Y la añadimos el numero a la coleccion
+			coleccion.add(numero);
+		}
+
+		//Mostramos el resultado al salir del bloque
+		System.out.println(coleccion);
+
+	}
+
+}
